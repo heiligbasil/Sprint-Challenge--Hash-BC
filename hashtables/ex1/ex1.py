bc3ed7b2c2a7581f5581698a6b3d8e2442dfb623
heiligbasil <basilief@yahoo.com>
2020-03-13 10:41:49 -0700

    Implement Hashtable example 1 - weight limit combination

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,18 +1,21 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_remove, hash_table_retrieve, hash_table_resize)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
+    if length == 2 and (weights[0] + weights[1] == limit):
+        return (1, 0)
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+    for w in weights:
+        retrieve_index = hash_table_retrieve(ht, limit - w)
+        if retrieve_index is not None:
+            this_index = hash_table_retrieve(ht, w)
+            if retrieve_index < this_index:
+                return (this_index, retrieve_index)
+            else:
+                return (retrieve_index, this_index)
     return None
 
 
