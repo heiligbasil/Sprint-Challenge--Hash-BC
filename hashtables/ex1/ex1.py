fde0e22753c4094ed29a0d952d1c6278e395b294
heiligbasil <basilief@yahoo.com>
2020-03-13 13:36:01 -0700

    Added additional solution alternatives from TL

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -19,6 +19,18 @@ def get_indices_of_item_weights(weights, length, limit):
     return None
 
 
+def get_indices_of_item_weights_alternative(weights, length, limit):
+    ht = HashTable(16)
+    for i in range(length):
+        complement = hash_table_retrieve(ht, limit - weights[i])
+        if complement is not None:
+            answer = (i, complement)
+            return answer
+        else:
+            hash_table_insert(ht, weights[i], i)
+    return None
+
+
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,3 +18,16 @@ def reconstruct_trip(tickets, length):
         route[i] = destination
         destination = hash_table_retrieve(hashtable, destination)
     return route[:-1]
+
+
+def reconstruct_trip_alternative(tickets, length):
+    hashtable = HashTable(length)
+    route = [None] * length
+    for i in range(length):
+        if tickets[i].source == "NONE":
+            route[0] = tickets[i].destination
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+    for j in range(length):
+        if route[j - 1] is not None:
+            route[j] = hash_table_retrieve(hashtable, route[j - 1])
+    return route[:-1]
